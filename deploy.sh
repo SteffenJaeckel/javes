f1c815d9b7bb17b4c29c08a9fd6429b8cd2a0e17
Steffen Jäckel <jaeckel@gmx.net>
2015-09-26 17:27:42 +0200

    Deploymentscript angepasst. Versionsdatei nach src verschoben.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,21 @@
 echo "prepare ..."
 VERSION=$(node inc_version.js)
 cd src/
-meteor bundle --architecture os.linux.x86_64 ../deployme.tar.gz
-cd ..
-git add deployme.tar.gz
+echo "Make build"
+pwd
+meteor build --directory ../build/ubuntu --architecture os.linux.x86_64
+cd ../build/ubuntu
+pwd
+echo "Delete old bundle.tar"
+rm bundle.tar
+echo "Create new bundle.tar"
+tar -c --file bundle.tar bundle
+echo "Delete bundle dir"
+rm -R -f bundle
+cd ../..
+pwd
+echo "Add new files to git"
+git add build/ubuntu/bundle.tar
 git add src/version.js
 git commit -m "$VERSION erstellt."
 git tag $VERSION
