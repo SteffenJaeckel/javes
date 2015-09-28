4b64ab88d2a6481fbbb06cdd86958d2a9a69abe9
Steffen jäckel <jaeckel@gmx.net>
2015-09-28 17:01:00 +0200

    "Hallo Welt" text aus dem Jagdplanung/Drucken/Standzettel modul entfernt

--- src/client/modules/huntingplans/printing/innerpage.js
+++ src/client/modules/huntingplans/printing/innerpage.js
@@ -2,25 +2,15 @@ var currentEditor;
 
 Template.innerpage.helpers({
   content: function () {
-    return "<h1>Hallo</h1><p>welt ...</p>";
+    return "";
   }
 })
 
 Template.innerpage.events({
   'dblclick .editable':function ( e ) {
-    console.log('edit')
-    $(e.currentTarget).addClass('editing').trumbowyg({
-      autogrow: false,
-      btns: ['formatting',
-      '|', 'btnGrp-design',
-      '|', 'btnGrp-justify',
-      '|', 'btnGrp-lists']
-    });
+    
   },
   'blur .editing': function ( e ) {
-    console.log("save edits ...");
-    $('.editing').trumbowyg('destroy');
-    $(e.currentTarget).removeClass('editing');
 
   }
 })
